99416fd4eea33cf2e631610b80131dc5a9c211e1
kyle-huynh23 <huynhkyle23@gmail.com>
2025-01-27 21:51:58 -0800

    added functionality gemini

--- app/page.js
+++ app/page.js
@@ -1,6 +1,27 @@
+"use client";
+
+
 import Image from "next/image";
+import { useState } from 'react';
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const secretKey = process.env.NEXT_PUBLIC_API_KEY;
+
+const genAI = new GoogleGenerativeAI(secretKey);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+const prompt = "Explain how AI works";
+
+
 
 export default function Home() {
+  const [clickCount, setClickCount] = useState(0);
+
+
+  const handleClick = async () => {
+    const result = await model.generateContent(prompt);
+    console.log(result.response.text());
+    setClickCount((prevCount) => prevCount + 1);
+  };
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -24,21 +45,9 @@ export default function Home() {
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
+        <button onClick={handleClick} className="button">
+          Click me!
+        </button>
           <a
             className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
